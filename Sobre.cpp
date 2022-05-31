bc5c2bcb9eaea843a4eb087c7079a22d5f7e37c9
CruzPer823 <105245863+CruzPer823@users.noreply.github.com>
2022-05-31 02:56:04 -0500

    Add files via upload

--- Sobre.cpp
+++ Sobre.cpp
@@ -1,22 +1,33 @@
 #include "Sobre.h"
-Sobre::Sobre(std::string _nombre, std::string _direccion, 
-std::string _ciudad,std::string _estado, int _codigo_postal,
-double _costo,double _largo,double _ancho, double _cargoa){
-nombre=_nombre;
-direccion=_direccion;
-ciudad=_ciudad;
-estado=_estado;
-codigo_postal=_codigo_postal;
-costo=_costo;
-cargoa=_cargoa;
-largo=_largo;
-ancho=_ancho;
+#include <iostream>
+Sobre::Sobre():Envio(){
+
+}
+Sobre::Sobre(std::string _nombreR, std::string _direccionR, 
+std::string _ciudadR, std::string _estadoR, int _codigo_postalR,
+std::string _nombreD, std::string _direccionD, std::string _ciudadD, 
+std::string _estadoD, int _codigo_postalD, double _costo, double _cargoa,
+double _largo,double _ancho):Envio(_nombreR,_direccionR,_ciudadR,_estadoR,
+     _codigo_postalR,_nombreD,_direccionD,_ciudadD, 
+    _estadoD, _codigo_postalD,_costo){
+    cargoa=_cargoa;
+    largo=_largo;
+    ancho=_ancho;
 }
 double Sobre::calculaCosto(double _largo, double _ancho){
-   if(_largo>25 or ancho>30){
-       costo
-   } 
+   if(_largo>25 || ancho>30){
+       return costo+=cargoa;
+    }else{
+        return costo;
+    } 
 }
-std::string imprimeDatos(){
-    
+std::string Sobre::imprimirDatos(){
+    std::cout<<"\n***DATOS DEL ENVIO***\n\n"<<"-Remitente-\n"
+  <<"Nombre: "<<nombreR<<"\n"<<"Estado: "<<estadoR<<"\n"
+  <<"Ciudad: "<<ciudadR<<"\n"<<"Direccion: "<<direccionR<<"\n"
+  <<"Codigo postal: "<<codigo_postalR<<"\n\n"<<"-Destinatario-\n"
+  <<"Nombre: "<<nombreD<<"\n"<<"Estado: "<<estadoD<<"\n"
+  <<"Ciudad: "<<ciudadD<<"\n"<<"Direccion: "<<direccionD<<"\n"
+  <<"Codigo postal: "<<codigo_postalD<<"\n\n"<<"***SOBRE***\n"
+  <<"Largo: "<<largo<<" cm\n"<<"Ancho: "<<ancho<<" cm\n";
 }
--- Sobre.h
+++ Sobre.h
@@ -6,9 +6,13 @@ class Sobre: public Envio{
     protected:
         double cargoa, largo, ancho;
     public:
-        Sobre(std::string, std::string, std::string, 
-        std::string, int,double,double,double,double);
+        Sobre();
+        Sobre(std::string _nombreR, std::string _direccionR, 
+        std::string _ciudadR, std::string _estadoR, int _codigo_postalR,
+        std::string _nombreD, std::string _direccionD, std::string _ciudadD, 
+        std::string _estadoD, int _codigo_postalD, double _costo
+        ,double cargoa,double largo, double ancho);
         double calculaCosto(double, double);
-        std::string imprimeDatos();
+        std::string imprimirDatos();
 };
 #endif
